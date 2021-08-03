e520d95f6d7465e63cadf44bcbc85948dfb24818
KaranjitKaurKK <karanjit.kaur@avanade.com>
2021-08-03 11:59:13 +0200

    Fifth commit: adjusted navbar's icon

--- src/app/core/componentes/navbar/navbar.component.ts
+++ src/app/core/componentes/navbar/navbar.component.ts
@@ -4,31 +4,26 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-navbar',
   template: `
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" routerLink='/home'>BEERVANADE</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarText">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" routerLink='/home'> Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" routerLink='/profile'> Profile </a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" routerLink='/catalog'> Catalog </a>
-      </li>
-    </ul>
-    <span class="navbar-text" style="float: right">
-      <a class="nav-link navbar-right" style="float: right" routerLink='/cart'><i class="fas fa-shopping-cart"></i> Cart </a>  
-    </span>
-  </div>
-</nav>
+    <a class="navbar-brand" routerLink='/home'>BEERVANADE</a>   
+    <div class="collapse navbar-collapse" id="navbarText">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <a class="nav-link" routerLink='/home'> Home <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" routerLink='/profile'> Profile </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" routerLink='/catalog'> Catalog </a>
+        </li>
+      </ul>
+        <a class="nav-link"  routerLink='/cart'>Cart <i class="fa fa-shopping-cart"></i></a>
+    </div>
+  </nav>
   <div class='container'>
     <router-outlet></router-outlet>
   </div>
-  `,
+`,
   styles: [
   ]
 })
